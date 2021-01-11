7301b1c49627a08d7287be006ee539a915006586
YarenCeker <77007935+YarenCeker@users.noreply.github.com>
2021-01-11 16:56:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İkiSayiArasindakiAsalSayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i <= 10000; i++)
+            {
+                bool SayiasalMi = true;
+                for (int bolen = 2; bolen < i; bolen++)
+                {
+                    if (i % bolen == 0)
+                    {
+                        SayiasalMi = false;
+                        break;
+                    }
+                }
+                if (SayiasalMi)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
